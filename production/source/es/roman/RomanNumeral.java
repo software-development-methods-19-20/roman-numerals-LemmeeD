24b85b63e5aac2f68982199c619c3029e3d37b72
Lorenzo <Slipknot_9>
2019-11-12 09:44:55 +0100

    We now convert 1 to I

---
+++ production/source/es/roman/RomanNumeral.java
@@ -0,0 +1,18 @@
+package es.roman;
+
+public class RomanNumeral {
+    private int decimal;
+
+    public RomanNumeral(int decimal) {
+
+        this.decimal = decimal;
+    }
+
+    @Override
+    public String toString() {
+        if (this.decimal == 0)
+            return "";
+        else
+            return "I";
+    }
+}
--- test/source/test/roman/DecimalToRomanNumeralTest.java
+++ test/source/test/roman/DecimalToRomanNumeralTest.java
@@ -1,5 +1,21 @@
 package test.roman;
+import es.roman.RomanNumeral;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import javax.naming.NamingException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DecimalToRomanNumeralTest {
 
+    @Test
+    void zeroIsEmptyString() {
+        RomanNumeral romanNumeral = new RomanNumeral(0);
+        assertEquals("", romanNumeral.toString());
+    }
+
+    @Test
+    void oneIsI() {
+        RomanNumeral romanNumeral = new RomanNumeral(1);
+        assertEquals("I", romanNumeral.toString());
+    }
 }
